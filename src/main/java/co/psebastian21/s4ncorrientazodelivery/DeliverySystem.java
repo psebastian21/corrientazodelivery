89d22e77cece12f59288298040bd578927b67b0e
Paulo <psebastian21@gmail.com>
2020-08-07 21:32:35 -0500

    advances in delivery system

--- src/main/java/co/psebastian21/s4ncorrientazodelivery/DeliverySystem.java
+++ src/main/java/co/psebastian21/s4ncorrientazodelivery/DeliverySystem.java
@@ -13,7 +13,6 @@ public class DeliverySystem {
 	private static int maxCargo = 3;
 
 	private List<Drone> drones;
-	private FileIO fileIO;
 	private String[] files = {
 			"in01.txt",	"in02.txt",	"in03.txt",	"in04.txt",	"in05.txt",	"in06.txt",	"in07.txt",	"in08.txt",
 			"in09.txt",	"in10.txt",	"in11.txt",	"in12.txt",	"in13.txt",	"in14.txt",	"in15.txt",	"in16.txt",
@@ -37,6 +36,7 @@ public class DeliverySystem {
 	}
 	
 	public DeliverySystem() throws IOException {
+		FileIO fileIO = new FileIO();
 		for(int i = 0; i < DeliverySystem.maxDrones; i++) {
 			List<String> instructions = fileIO.readFileLines(files[i]);
 			Drone drone = new Drone(instructions);
