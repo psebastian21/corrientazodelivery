b67f2eecf31c09e9bd130bf1005a32e0c344216b
Paulo <psebastian21@gmail.com>
2020-08-07 20:49:48 -0500

    advances in Main

---
+++ src/main/java/co/psebastian21/s4ncorrientazodelivery/Main.java
@@ -0,0 +1,12 @@
+package co.psebastian21.s4ncorrientazodelivery;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to S4N's Corrientazo Delivery app 1.0!");
+		System.out.println("Proceeding with default values");
+		
+		
+	}
+
+}
