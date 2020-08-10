5fac4b7eafffa8fe080b8c5b5ea1bafe89f799cf
Paulo <psebastian21@gmail.com>
2020-08-10 14:50:46 -0500

    More unit tests

--- src/test/java/co/psebastian21/s4ncorrientazodelivery/io/FileIOTests.java
+++ src/test/java/co/psebastian21/s4ncorrientazodelivery/io/FileIOTests.java
@@ -1,6 +1,8 @@
 package co.psebastian21.s4ncorrientazodelivery.io;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
@@ -23,6 +25,40 @@ public class FileIOTests {
 		List<String> output = fileIO.readFileLines("");
 		//Assert
 		Assert.assertEquals(expected, output);
+	}
+	@Test
+	public void whenWritingFileThenFileIsWritten() throws IOException {
+		//Arrange
+		FileIO fileIO = Mockito.spy(new FileIO());
+		File file = Mockito.mock(File.class);
+		Mockito.when(file.exists()).thenReturn(true);
+		FileWriter writer = Mockito.mock(FileWriter.class);
+		Mockito.doNothing().when(writer).write(Mockito.anyString());
+		Mockito.doReturn(file).when(fileIO).instantiateFile(Mockito.anyString());
+		Mockito.doReturn(writer).when(fileIO).instantiateFileWriter(Mockito.any(File.class), Mockito.anyBoolean());
+		//Act
+		fileIO.writeFile("", "");
+		//Assert
+		Mockito.verify(file).exists();
+		Mockito.verify(writer).write(Mockito.anyString());
+	}
+	@Test
+	public void whenFileDoesntExistThenCreateAndWrite() throws IOException {
+		//Arrange
+		FileIO fileIO = Mockito.spy(new FileIO());
+		File file = Mockito.mock(File.class);
+		Mockito.when(file.exists()).thenReturn(false);
+		FileWriter writer = Mockito.mock(FileWriter.class);
+		Mockito.doNothing().when(writer).write(Mockito.anyString());
+		Mockito.doReturn(file).when(fileIO).instantiateFile(Mockito.anyString());
+		Mockito.doReturn(writer).when(fileIO).instantiateFileWriter(Mockito.any(File.class), Mockito.anyBoolean());
+		Mockito.when(file.createNewFile()).thenReturn(true);
+		//Act
+		fileIO.writeFile("", "");
+		//Assert
+		Mockito.verify(file).exists();
+		Mockito.verify(writer).write(Mockito.anyString());
+		Mockito.verify(file).createNewFile();
 	}
 
 }
